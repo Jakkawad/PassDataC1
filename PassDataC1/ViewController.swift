e60d3d9a9fb854ebfd60061853b91022e7ceacf0
Jakkawad <admin@all2sale-imac.local>
2016-03-28 15:29:15 +0700

    Initial Commit

---
+++ PassDataC1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassDataC1
+//
+//  Created by admin on 3/28/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
